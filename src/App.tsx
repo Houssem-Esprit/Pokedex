4becb038fba046badb16a6c7e932900147cbddb5
Houssem-Esprit <houssemeddine.rkaiess@esprit.tn>
2022-03-15 14:01:04 +0100

    init project

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App-header">
+      <header className="header">
+        <p className='logo'>Pokedex | <span className='logo'> 
+          <img src='https://res2.weblium.site/res/5e6114c4d12f9f00211ad416/6091cb10305c9b00226c88ab_optimized_450?nowebp' 
+            alt='' width="150px"/> 
+          </span></p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
